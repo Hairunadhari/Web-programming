2674d70ac802514ab435741fd5b2f85c451585a3
unknown <hairunadhari@gmail.com>
2024-11-12 22:03:14 +0700

    landing page & templet admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [HomeController::class, 'index']);
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'dashboard']);
 });
